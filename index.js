08e0fb4a2c928d6d3c771263acacda1e8d714f4f
akshaypandey28 <akshaychaman2004@gmail.com>
2026-05-09 16:00:58 +0530

    setup authentication API Gateway

--- index.js
+++ index.js
@@ -16,6 +16,45 @@ const limiter = rateLimit({ // i will allow only maximum 5 request from an IP wi
 app.use(morgan('combined'));
 app.use(limiter);
 
+app.use('/bookingservice', async (req, res, next) => {
+    const token = req.headers['x-access-token'];
+    
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message: 'Token not found'
+        });
+    }
+
+    try {
+        const response = await axios.get('http://localhost:3001/api/v1/isAuthenticated',
+            {
+                headers: {
+                    'x-access-token': token
+                },
+                timeout: 5000 //How long axios should wait for response before failing
+            }
+        );
+
+        console.log(response.data);
+
+        if (response.data.success === true) {
+            return next();
+        }
+
+        return res.status(401).json({
+            success: false,
+            message: 'Unauthorised'
+        });
+
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: 'Unauthorised in Catch'
+        });
+    }
+});
+
 app.use('/bookingservice', createProxyMiddleware({
     target: 'http://localhost:3002',
     changeOrigin: true
